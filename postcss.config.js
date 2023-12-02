3d43ab2e11be45004f1f5a18c0568801867d445c
debianAdmin <sebamgb@gmail.com>
2023-12-01 22:30:16 -0300

    packages: Added dependecies postcss and @fullhuman/postcss-purgecss and appling them

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,23 @@
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [
-    // the following runs when env is NOT development (production)
-    !process.env.HUGO_ENVIRONMENT === 'development'
-    ? autoprefixer()
-    : null,
-  ]
-}
+  plugins: {
+    '@fullhuman/postcss-purgecss': {
+      content: [ 
+        "./layouts/**",
+        "./content/**",
+      ],
+      safelist: [
+        "carousel-item-start",
+        "carousel-item-next",
+        "d-none",
+        "was-validated",
+        "alert",
+        "alert-danger",
+        "alert-success",
+        "spinner-border",
+      ],
+    },
+    autoprefixer: {},
+  },
+};
